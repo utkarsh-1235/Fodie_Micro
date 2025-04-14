57fbe2a7ff2ac405160cfc668c3d3b2bf5032a1a
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-04-14 20:19:12 +0530

    Modifying Event bus

---
+++ event-bus/index.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const Port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(Port, (req, res)=>{
+    console.log(`Event is Running on Port ${Port}`);
+})
