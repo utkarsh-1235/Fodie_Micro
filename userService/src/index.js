8fc62319c6390758e315d75de43133053e74339f
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-04-26 21:05:10 +0530

    changes to be committed

--- userService/src/index.js
+++ userService/src/index.js
@@ -5,15 +5,22 @@ const SignUpRoute = require('./SignUpRoute');
 const logoutRoute = require('./logoutRoute');
 const LoginRoute = require('./LoginRoute');
 const addressRoute = require('./AddressRoute');
+const cookieSession = require('cookie-session');
 const SignUp = require('./SignUpRoute');
 const morgan = require('morgan');
+const { trusted } = require('mongoose');
 const app = express();
 const Port = process.env.PORT;
 
+app.set('trust proxy', true); // trust first proxy
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
+app.use(cookieSession({
+    signed: false,
+    secure: trusted
+}));
 app.use('/api/users',SignUp);
 app.use('/api/users',LoginRoute);
 app.use('/api/users',addressRoute);
