4a924ccde80695fa1e7b9ab5e1688af932765084
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-05-01 21:58:00 +0530

    Adding userAuth Middleware in order service and modifying order service

--- orderService/src/index.js
+++ orderService/src/index.js
@@ -1,13 +1,22 @@
 const express = require('express'); 
 const dbConnect = require('./db');
-const orderRoute = require('./orderRoute');
+const orderRoute = require('./CreateorderRoute');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
-
+const {trusted} = require('mongoose');
 const app = express();
 const Port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use(cookieSession({
+    signed: false,
+    secure: trusted
+}));
 app.use(bodyParser.json());
 
 app.use('/api/orders',orderRoute);
---
+++ orderService/src/orderController.js
@@ -0,0 +1,36 @@
+// const orderModel = require('./orderSchema');
+// const userModel = require('../userService/userSchema');
+
+
+
+// const getUserOrder = async(req, res)=>{
+//    try{
+//     const {userId}= req.body.userId;
+
+//     if(!userId){
+//         return res.status(400).json('User id required');
+//     }
+
+//     const orders = await orderModel.find({'user.userId': userId}).populate('items.dish', 'name price image');
+
+//     if(orders.length === 0){
+//         return res.status(401).json('No Orders found please order something');
+//     }
+
+//     return res.status(200).json({
+//         succes: true,
+//         message: 'Orders fetched succesfully',
+//         Orders: Array.isArray(orders) ? orders : []
+//     })
+
+//    }catch(err){
+//     res.status(500).json({
+//         success: false,
+//         message: err.message
+//     })
+//    }
+// }
+
+// module.exports = {
+//                    getUserOrder
+// }
