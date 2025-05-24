ebe08f80530811b022d3dad973f3634d9cbae1e1
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-05-24 23:25:54 +0530

    modifyng server.js

--- userService/src/server.js
+++ userService/src/server.js
@@ -1,8 +1,22 @@
-const app = require('./index'); // import the Express app
-const dbConnect = require('./db');
+// src/server.ts
+import dotenv from 'dotenv';
+import { app } from './app';
+import { connectToDB } from './db';
+
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, async () => {
-  await dbConnect();
-  console.log(`User Service is running on port ${PORT}`);
-});
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  await connectToDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+start();
